2ce4c8ebf23101042b4aa8d0eb9a0cbd680085b6
N8-Doge <nathantchennnttcc@gmail.com>
2019-02-28 10:07:22 -0800

    minor change

--- Dossier/Menu.java
+++ Dossier/Menu.java
@@ -8,14 +8,18 @@ import javax.swing.*;
  */
 public class Menu extends Window
 {
+    private static JButton viewButton, remindButton, newButton;
     public Menu()
     {
         super("Menu");
         JPanel center = new JPanel();
         center.setLayout(new GridLayout(3,1));
-        center.add(new JButton("View Schedule"));
-        center.add(new JButton("Reminders"));
-        center.add(new JButton("New Item"));
+        viewButton = new JButton("View Schedule");
+        center.add(viewButton);
+        remindButton = new JButton("Reminders");
+        center.add(remindButton);
+        newButton = new JButton("New Item");
+        center.add(newButton);
         add(center, BorderLayout.CENTER);
     }
 }
