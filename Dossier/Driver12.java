1860873b4daed13ca6d70c747d52d7bff9af3486
N8-Doge <nathantchennnttcc@gmail.com>
2019-02-27 18:15:11 -0800

    Created files

---
+++ Dossier/Driver12.java
@@ -0,0 +1,26 @@
+import java.awt.*;
+import java.util.*;
+import javax.swing.*;
+/**
+ * Used to start up jar file, template from torbert's driver
+ * 
+ * @author      Nathan Chen
+ * @version     2-27-19
+ */
+public class Driver12
+{
+    /**
+     * Runs program
+     * 
+     * @param   args    For jar, ignore
+     */
+    public static void main(String[] args)
+    {
+        JFrame frame = new JFrame("My Schedule");
+        frame.setSize(400, 400);
+        frame.setLocation(200, 100);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setContentPane(new JPanel());
+        frame.setVisible(true);
+    }
+}
